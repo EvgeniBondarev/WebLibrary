cd38e45b1549c5947de783362832e11caff5ff41
EvgeniBondarev <bond.evg03@mail.ru>
2023-10-17 20:26:48 +0300

    To hosting Heroku

--- web_library/settings.py
+++ web_library/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
